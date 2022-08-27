ad91d2399bb7d2a0325d7732561460c404450c0a
Mujtaba Alkhalifah <mojtaba989@gmail.com>
2022-08-27 03:27:37 +0300

    Removed Google Client Api library: no longer using the google-api-python-client library, instead making requests using httpx for asynchronous requests and no typing issues.

--- src/youtube_comment_collecter/__main__.py
+++ src/youtube_comment_collecter/__main__.py
@@ -1,70 +1,66 @@
 from __future__ import annotations
 
+import asyncio
 import os
 
-import googleapiclient.discovery
-import googleapiclient.errors
 from dotenv import load_dotenv
+import httpx
 
 from youtube_comment_collecter import constants
 from youtube_comment_collecter.api_resources.comment_thread_resource import (
     CommentThreadListResponse,
     CommentThreadResource,
 )
-from youtube_comment_collecter.video import Video
 
 
-def main() -> int:
+async def main() -> int:
     load_dotenv()
 
     developer_key = os.environ[constants.GOOGLE_APPLICATION_API_KEY_ENV]
+    api_url = "https://www.googleapis.com/youtube/v3/commentThreads"
+    video_id = "_VB39Jo8mAQ"
 
-    youtube = googleapiclient.discovery.build(
-        serviceName=constants.API_SERVICE_NAME,
-        version=constants.API_VERSION,
-        developerKey=developer_key,
-    )
-
-    video = Video(id="_VB39Jo8mAQ")
-
-    comment_thread_handler = youtube.commentThreads()
-    comment_thread_response: CommentThreadListResponse = (
-        comment_thread_handler.list(
-            part="id,snippet,replies",
-            videoId=video.id,
-            maxResults=100,
-            textFormat="plainText",
-        ).execute()
-    )
-
-    comment_threads: list[CommentThreadResource] = []
-    comment_threads.extend(comment_thread_response["items"])
-
-    while comment_thread_response.get("nextPageToken") is not None:
-        next_page_token = comment_thread_response.get("nextPageToken")
-        comment_thread_response = comment_thread_handler.list(
-            part="id,snippet,replies",
-            videoId="_VB39Jo8mAQ",
-            maxResults=100,
-            textFormat="plainText",
-            pageToken=next_page_token,
-        ).execute()
-
+    comment_threads: list[CommentThreadResource] = list()
+    async with httpx.AsyncClient() as client:
+        request_params: dict[str, str | int | bool] = {
+            "key": developer_key,
+            "videoId": video_id,
+            "part": "id,snippet,replies",
+            "maxResults": 100,
+            "textFormat": "plainText",
+            "pageToken": "",
+        }
+        response = await client.get(
+            url=api_url,
+            params=request_params,
+        )
+        comment_thread_response: CommentThreadListResponse = response.json()
         comment_threads.extend(comment_thread_response["items"])
 
-    for i, thread in enumerate(comment_threads):
-        comment_snip = thread["snippet"]["topLevelComment"]["snippet"]
-        if comment_snip["likeCount"] > 0:
-
-            print(
-                f"[{i}] [likes: {comment_snip['likeCount']}] [published at:"
-                f" {comment_snip['publishedAt']}]"
+        while comment_thread_response.get("nextPageToken") is not None:
+            next_page_token = comment_thread_response.get("nextPageToken")
+            request_params["pageToken"] = (
+                next_page_token if next_page_token is not None else ""
             )
-            print(comment_snip["textOriginal"])
-            print("*" * 120)
+            response = await client.get(url=api_url, params=request_params)
+            comment_thread_response = response.json()
+            comment_threads.extend(comment_thread_response["items"])
+
+    for i, thread in enumerate(comment_threads):
+        comment_snippent = thread["snippet"]["topLevelComment"]["snippet"]
+        if comment_snippent["likeCount"] > 0:
+            print(
+                f"[{i}] [likes: {comment_snippent['likeCount']}] [published"
+                f" at: {comment_snippent['publishedAt']}]"
+            )
+            print(comment_snippent["textOriginal"])
+        else:
+            print("Skipping!!!")
+
+        print("*" * 120)
 
     return 0
 
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    raise SystemExit(asyncio.run(main()))
