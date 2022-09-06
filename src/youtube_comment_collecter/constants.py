f4d6538e3449fb9ff64fd289b5278056777a161f
Mujtaba Alkhalifah <mojtaba989@gmail.com>
2022-09-06 22:00:37 +0300

    Removed unused constants

--- src/youtube_comment_collecter/constants.py
+++ src/youtube_comment_collecter/constants.py
@@ -1,9 +1,6 @@
 from __future__ import annotations
 
 GOOGLE_APPLICATION_API_KEY_ENV = "GOOGLE_APPLICATION_API_KEY"
-SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
-API_SERVICE_NAME = "youtube"
-API_VERSION = "v3"
 
 VIDEO_API_URL = "https://youtube.googleapis.com/youtube/v3/videos"
 THREADS_API_URL = "https://www.googleapis.com/youtube/v3/commentThreads"
